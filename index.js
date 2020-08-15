477c32ba0f9bae6472135765e840f653754a10df
dahye-program <dahye0729@sch.ac.kr>
2020-08-16 02:59:57 +0900

    object and array

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Im working. Im JS. Im Beautiful. Im worth it');
+//주석
+const a=221; //변하지 않는 변수
+let b=a-5;
+console.log('a b');
+
+//array
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+console.log(daysOfWeek);
+
+//object
+const nicoInfo={
+    name:"Nico",
+    age:33,
+    gender:"Male",
+    isHandsome:true,
+    favMovies: ["Along the gods","Oldboy"],
+    favFood: [
+      {
+        name: "kimchi",
+        fatty:false
+      },
+      {
+        name:"Cheese burger",
+        fatty:true
+      }
+    ]
+}
+
+console.log(nicoInfo);
