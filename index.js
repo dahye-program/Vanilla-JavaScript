6ae08834145086994dde7ff2e97b14cadfeb1ed7
dahye-program <dahye0729@sch.ac.kr>
2020-08-17 01:29:46 +0900

    function practice

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-//DOM(Document Object Module)
-const title = document.querySelector("#title");
-title.innerHTML="Hi! From JS";
-title.style.color="red";
-document.title = "I own you now";
-
-//Events and event handlers
-function handleResize(){
-    console.log("I have been resized")
-}
-
-window.addEventListener("resize", handleResize); // 윈도우 사이즈가 변경될 때 함수 호출됨
-window.addEventListener("resize", handleResize()); // 즉시 함수 호출됨
-
-//argument = event
 const title = document.querySelector("#title");
 
-function handleResize(event){
-    console.log(event);
-}
-window.addEventListener("resize",handleResize);
-
-//click
-const title = document.querySelector("#title");
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
 
 function handleClick(){
-    title.syle.color = "red";
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }else{
+        title.style.color = BASE_COLOR;
+    }
 }
 
-title.addEventListener("click",handleClick);
-
-//if-else, prompt
-const age = prompt("How old are you?");
-
-if(age>18&&age<21){
-    console.log("you can drink but you should not");
-}else{
-    console.log("you cant");
+function init(){
+    title.style.color=BASE_COLOR;
+    title.addEventListener("click", handleClick);
 }
+
+init();
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS="clicked";
+
+function handleClick(){
+    const currentClass=title.className; //가져와
+    if(currentClass!==CLICKED_CLASS){
+        title.className=CLICKED_CLASS; //값 세팅
+    }else{
+        title.className="";
+    }
+}
+
+function init(){
+    title.style.color=BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+
+init();
