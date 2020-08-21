4e8e0aab67a4882d721ad0af1ad9f55af11657ea
dahye <dahye0729@sch.ac.kr>
2020-08-22 00:32:33 +0900

    show clock

--- clock.js
+++ clock.js
@@ -4,13 +4,15 @@ const clockTitle = clockContainer.querySelector("h1");
 
 function getTime(){
     const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    const minutes = date.getMinutes(); //분
+    const hours = date.getHours(); //시
+    const seconds = date.getSeconds(); //초
+    clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}`:minutes}:${seconds < 10 ? `0${seconds}`: seconds}`; // 00:00:00 으로 표기하기 위함
+    // 조건 ? 참일때 실행 : 그렇지 않을 때 실행
 }
 
 function init(){
     getTime();
+    setInterval(getTime,1000);
 }
 init();
