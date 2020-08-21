1b0bbdb4e7c8ac37d46fe11d56ae424415484d6b
dahye <dahye0729@sch.ac.kr>
2020-08-22 01:00:14 +0900

    벌레 너무 많아 안해

---
+++ gretting.js
@@ -0,0 +1,38 @@
+const form = document.querySelector(".js-form")
+const input = form.querySelector("input")
+const gretting = document.querySelector("".js-gretting);
+
+const USER_LS = "currentUser"
+const SHOWINT_CN= "showing"
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWINT_CN);
+    form.addEventListener("submit",handleSubmit);
+}
+
+function paintGreeting(text){
+    form.classList.remove(SHOWINT_CN);
+    gretting.classList.add(SHOWINT_CN);
+    gretting.innerText = `Hello ${text}`
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser===null){
+        askForName();
+    }else{
+        paintGreeting(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
